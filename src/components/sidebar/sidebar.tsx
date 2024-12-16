0a147601c420acd6fa0ae609d903a6c87259dbb6
Hakiem <hakiem@hakiems-air.lan>
2024-12-16 22:58:11 +0800

    Side bar UI

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,29 +1,101 @@
 import "./sidebar.css";
+import DQIcon from "../../assets/images/general/dq-logo.png";
+import ShopIcon from "../../assets/images/sidebar/shop.png";
+import AvatarIcon from "../../assets/images/sidebar/avatar.png";
+import InventoryIcon from "../../assets/images/sidebar/bag.png";
+import SkillsIcon from "../../assets/images/sidebar/skills.png";
+import FarmingIcon from "../../assets/images/sidebar/farm.png";
+import CraftingIcon from "../../assets/images/sidebar/craft.png";
+import SlimeLabIcon from "../../assets/images/sidebar/slime-lab.png";
+import CombatIcon from "../../assets/images/sidebar/combat.png";
 
 interface SidebarProps {
-    isOpen: boolean;
-    toggleSidebar: () => void;
-    setPage: (page: string) => void;
+  isOpen: boolean;
+  toggleSidebar: () => void;
+  setPage: (page: string) => void;
 }
 
 function Sidebar({ isOpen, toggleSidebar, setPage }: SidebarProps) {
-    return (
-        <div className={`sidebar ${isOpen ? "open" : ""}`}>
-            <button className="close-btn" onClick={toggleSidebar}>✕</button>
-            <ul>
-                <li><a onClick={() => setPage("Shop")}>Shop</a></li>
-                <li className="gap"></li>
-                <li><a onClick={() => setPage("Avatar")}>Avatar</a></li>
-                <li><a onClick={() => setPage("Inventory")}>Inventory</a></li>
-                <li><a onClick={() => setPage("Skills")}>Skills</a></li>
-                <li><a onClick={() => setPage("Farming")}>Farming</a></li>
-                <li><a onClick={() => setPage("Crafting")}>Crafting</a></li>
-                <li><a onClick={() => setPage("Slime Lab")}>Slime Lab</a></li>
-                <li><a onClick={() => setPage("Combat")}>Combat</a></li>
-                <li><a onClick={() => setPage("Test")}>Test</a></li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
+      <button className="close-btn" onClick={toggleSidebar}>
+        ✕
+      </button>
+      <div className="dq-icon-container">
+        <img src={DQIcon} alt="Game Icon" className="dq-icon" />
+      </div>
+      <ul>
+        <li>
+          <a onClick={() => setPage("Shop")}>
+            <img src={ShopIcon} alt="Shop Icon" className="sidebar-icon" />
+            Shop
+          </a>
+        </li>
+        <li>
+          <a onClick={() => setPage("Avatar")}>
+            <img src={AvatarIcon} alt="Avatar Icon" className="sidebar-icon" />
+            Avatar
+          </a>
+        </li>
+        <li>
+          <a onClick={() => setPage("Inventory")}>
+            <img
+              src={InventoryIcon}
+              alt="Inventory Icon"
+              className="sidebar-icon"
+            />
+            Inventory
+          </a>
+        </li>
+        <li>
+          <a onClick={() => setPage("Skills")}>
+            <img src={SkillsIcon} alt="Skills Icon" className="sidebar-icon" />
+            Skills
+          </a>
+        </li>
+        <li>
+          <a onClick={() => setPage("Farming")}>
+            <img
+              src={FarmingIcon}
+              alt="Farming Icon"
+              className="sidebar-icon"
+            />
+            Farming
+          </a>
+        </li>
+        <li>
+          <a onClick={() => setPage("Crafting")}>
+            <img
+              src={CraftingIcon}
+              alt="Crafting Icon"
+              className="sidebar-icon"
+            />
+            Crafting
+          </a>
+        </li>
+        <li>
+          <a onClick={() => setPage("Slime Lab")}>
+            <img
+              src={SlimeLabIcon}
+              alt="Slime Lab Icon"
+              className="sidebar-icon"
+            />
+            Slime Lab
+          </a>
+        </li>
+        <li>
+          <a onClick={() => setPage("Combat")}>
+            <img src={CombatIcon} alt="Combat Icon" className="sidebar-icon" />
+            Combat
+          </a>
+        </li>
+        <div className="gap"></div>
+        <li>
+          <a onClick={() => setPage("Test")}>Test</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
