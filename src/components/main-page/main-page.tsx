81c5fdf7a6ff25b20710cdf5e976e9949f2ab109
Hakiem <hakiem@hakiems-air.lan>
2024-12-16 23:18:50 +0800

    Header icons for current page

--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -9,10 +9,31 @@ import SlimeLabPage from "../slime-lab/slime-lab";
 import FarmingPage from "../farming-page/farming-page";
 import AvatarPage from "../avatar-page/avatar-page";
 import Player from "./player/player";
+import ShopIcon from "../../assets/images/sidebar/shop.png";
+import AvatarIcon from "../../assets/images/sidebar/avatar.png";
+import InventoryIcon from "../../assets/images/sidebar/bag.png";
+import SkillsIcon from "../../assets/images/sidebar/skills.png";
+import FarmingIcon from "../../assets/images/sidebar/farm.png";
+import CraftingIcon from "../../assets/images/sidebar/craft.png";
+import SlimeLabIcon from "../../assets/images/sidebar/slime-lab.png";
+import CombatIcon from "../../assets/images/sidebar/combat.png";
 
 function MainPage() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState("Avatar");
+
+  // Mapping of pages to their respective icons
+  const pageIcons: { [key: string]: string } = {
+    Shop: ShopIcon,
+    Avatar: AvatarIcon,
+    Inventory: InventoryIcon,
+    Skills: SkillsIcon,
+    Farming: FarmingIcon,
+    Crafting: CraftingIcon,
+    "Slime Lab": SlimeLabIcon,
+    Combat: CombatIcon,
+    Test: ShopIcon, // Placeholder for Test
+  };
 
   function toggleSidebar() {
     setIsSidebarOpen(!isSidebarOpen);
@@ -51,10 +72,19 @@ function MainPage() {
   return (
     <div id="main-page-container" className="noselect">
       <header className="header">
-        <button className="open-sidebar-btn" onClick={toggleSidebar}>
+        <div className="open-sidebar-btn" onClick={toggleSidebar}>
           ☰
-        </button>
-        <div className="header-title">{currentPage}</div>
+        </div>
+        <div className="header-title">
+          {/* Render the icon and page name dynamically */}
+          {pageIcons[currentPage] && (
+            <img
+              src={pageIcons[currentPage]}
+              alt={`${currentPage} Icon`}
+              className="header-icon"
+            />
+          )}
+        </div>
         <Player />
       </header>
       <Sidebar
