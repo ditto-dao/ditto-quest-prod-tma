9f9d6dcdb075428796e8b883d58965db9a16ebeb
Hakiem <hakiem@Hakiems-Macbook-Air.local>
2025-04-15 17:19:04 +0800

    Configs for dq beta

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,18 @@
-export const SOCKET_ORIGIN = "https://dqdev.ditto.zone"
+/* export const SOCKET_ORIGIN = "https://dqdev.ditto.zone"
 
 export const SOCKET_PATH = "/ditto-quest-socket-adapter-dev"
+
+export const S3_SLIME_FOLDER = "slimes-test"
+
+export const DITTO_DECIMALS = 9
+
+export const DEVELOPMENT_FUNDS_KEY = "DEVELOPMENT_FUNDS"
+
+export const LEVERAGE_POOL_KEY = "LEVERAGE_POOL" */
+
+export const SOCKET_ORIGIN = "https://ditto-quest.ditto.zone"
+
+export const SOCKET_PATH = "/ditto-quest-socket-adapter"
 
 export const S3_SLIME_FOLDER = "slimes-test"
 
